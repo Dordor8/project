aababc373e658974b083bc4c732b63d6f5cc106d
Dordor8 <farfarador@gmail.com>
2026-03-04 18:44:55 +0200

    new_deployment is now working

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from sqlalchemy import inspect
 
 if "__main__" == __name__:
     m = MongoDBService()
+    print(m.new_deployment('test-my_db2', 'test'))
--- src/logic/manager.py
+++ src/logic/manager.py
@@ -2,8 +2,11 @@ from abc import ABC, abstractmethod
 from src.exceptions import DeploymentException
 from pymongo import MongoClient
 import configparser
-from sqlalchemy import create_engine, Column, INT, String, BOOLEAN, TIMESTAMP
+from sqlalchemy import create_engine, Column, INT, String, BOOLEAN, TIMESTAMP, text
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from uuid import uuid1
+import datetime
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -16,7 +19,7 @@ Base = declarative_base()
 
 class Deployment(Base):
     __tablename__ = 'deployments'
-    id = Column(INT, primary_key=True)
+    id = Column(String, primary_key=True)
     db_name = Column(String)
     status = Column(BOOLEAN)
     username = Column(String)
@@ -47,12 +50,29 @@ class DBService(ABC):
 
 class MongoDBService(DBService):
     def __init__(self):
-        self.client = MongoClient(f"mongodb://{config['DEPLOYMENTS_PRAM']['mongo_host']}:{config['DEPLOYMENTS_PRAM']['mongo_port']}")
+        self.client = MongoClient(
+            f"mongodb://{config['DEPLOYMENTS_PRAM']['mongo_host']}:{config['DEPLOYMENTS_PRAM']['mongo_port']}")
         Base.metadata.create_all(bind=engine)
-        print(Base.metadata.tables.keys())
+
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
 
     def new_deployment(self, db_name: str, username: str) -> str:
-        pass
+        if self.session.query(Deployment).filter(Deployment.db_name == db_name).first():
+            raise DeploymentException("Deployment name already exists")
+
+        if not db_name.startswith(username):
+            raise DeploymentException("Deployment name must start with the username")
+
+        deployment_id: str = str(uuid1())
+        self.session.add(Deployment(id=deployment_id, db_name=db_name, status=True, username=username,
+                                    created_at=datetime.datetime.now()))
+
+        db = self.client[deployment_id]
+        db.create_collection('holder')  # temp table so the db will create
+
+        self.session.commit()
+        return deployment_id
 
     def get_deployment_info(self, db_name: str, username: str) -> str:
         pass
