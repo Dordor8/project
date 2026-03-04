73f32d7a99b11df2ad5a0d89c73aab4708a49dc0
Dordor8 <farfarador@gmail.com>
2026-03-04 14:50:16 +0200

    monogodbservice

--- src/deployable/dbs.py
+++ src/deployable/dbs.py
@@ -1,23 +1,32 @@
 from abc import ABC, abstractmethod
 from typing import Any
-
+from pymongo import MongoClient
 
 class DBService(ABC):
     @abstractmethod
-    def connect(self):
+    def create_db(self, db_name: str) -> None:
         pass
-    @abstractmethod
-    def disconnect(self):
-        pass
-    @abstractmethod
-    def create_db(self, db_name: str, username: str) -> str:
-        pass
+
     @abstractmethod
     def drop_db(self, db_name: str) -> None:
         pass
-    @abstractmethod
-    def get_db(self, db_name: str) -> Any:
-        pass
-    @abstractmethod
-    def update_db(self, db_current_name: str, db_new_db_name: str) -> str:
-        pass
+
+
+class MongoDBService(DBService):
+    def __init__(self):
+        self.client = MongoClient("mongodb://localhost:27017")
+
+    def is_db_exist(self, db_name: str) -> bool:
+        return db_name in self.client.list_database_names()
+
+    def create_db(self, db_name: str) -> None:
+        if not self.is_db_exist(db_name):
+            db = self.client[db_name]
+        else:
+            raise Exception()
+
+    def drop_db(self, db_name: str) -> None:
+        if self.is_db_exist(db_name):
+            self.client.drop_database(db_name)
+        else:
+            raise Exception()
